ff41211ac756fe02dc76b9fa0bf4262460808496
TusyaZaharova <natalya.vaabel@yandex.ru>
2024-08-19 10:39:10 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+Finished_task = 12
+Number_of_hours_spent = 1.5
+Course_Title = 'Python'
+speed_of_task_execution = (Number_of_hours_spent / Finished_task)
+print('Курс: ', Course_Title, ', всего задач: ', Finished_task, ', затрачено часов: ', Number_of_hours_spent, ', среднее время выполнения задания: ', speed_of_task_execution, 'часа.')
